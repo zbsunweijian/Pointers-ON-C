fc5b216b47135e30525d76d17a7b294c1e64871e
zbsunweijian <zbsunwj@163.com>
2015-08-15 22:44:05 +0800

    find
    a number

---
+++ find number.cpp
@@ -0,0 +1,23 @@
+bool Find(int* matrix, int rows, int columns, int number)
+{
+	bool found = false;
+	
+	if(matrix != NULL && rows > 0 && columns >0)
+	{
+		int row = 0;
+		int column = columns - 1;
+		while(row < rows && column > 0)
+		{
+			if(matrix[row * columns + column] == number)
+			{
+				found = ture;
+				break;
+			}
+			else if(matrix[row * columns + column] > number)
+				--column;
+			else
+				++row;
+		}
+	}
+	return found;
+}
