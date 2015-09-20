d556e20691c0383f3b81a3d80a81e996dd1a8993
zbsunweijian <zbsunwj@163.com>
2015-09-20 22:37:10 +0800

    chat online
    chat online

---
+++ chat online.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main(void)
+{
+	int p,q,l,r;
+	int a[50] = {0};
+	int b[50] = {0};
+	int c[50] = {0};	
+	int d[50] = {0};
+	int i = 0;
+	int j = 0;
+	scanf("%d %d %d %d",&p,&q,&l,&r);
+	for(i = 0;i < p;i++)
+	{
+		scanf("%d %d",&a[i],&b[i]);
+	}
+	for(j = 0;j < q;j++)
+	{
+		scanf("%d %d",&c[j],&d[j]);
+	}
+
+
+}
